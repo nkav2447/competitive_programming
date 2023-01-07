2dbcaafebb3397002499f1974f5dcbcd03e07e61
nkav2447 <120913323+nkav2447@users.noreply.github.com>
2023-01-07 09:43:01 -0300

    Create implement_queue.py

---
+++ implement_queue.py
@@ -0,0 +1,33 @@
+class MyQueue(object):
+    def __init__(self):
+        self.push_stack = []
+        self.pop_stack = []
+    def push(self, x):
+        self.push_stack.append(x)
+    def pop(self):
+        if self.empty(): return
+        if len(self.pop_stack):
+            return self.pop_stack.pop()
+        else:
+            while len(self.push_stack):
+                self.pop_stack.append(self.push_stack.pop())
+        return self.pop_stack.pop()
+    def peek(self):
+        if self.empty(): return
+        if len(self.pop_stack):
+            return self.pop_stack[-1]
+        else:
+            while len(self.push_stack):
+                self.pop_stack.append(self.push_stack.pop())
+        return self.pop_stack[-1]
+    def empty(self):
+        return len(self.push_stack)==False and len(self.pop_stack) == False
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(value)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
