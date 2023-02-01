f921dd473d3254dedfd897d7ef51e3182cd31f96
nkav2447 <120913323+nkav2447@users.noreply.github.com>
2023-02-01 07:53:50 -0300

    added a better solution

--- longest_substr_without_rep.py
+++ longest_substr_without_rep.py
@@ -1,3 +1,18 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if s == None or len(s) == 0:
+            return 0
+        i, j, maximum = 0, 0, 0
+        chr_set = set()
+        while i < len(s):
+            while s[i] in chr_set:
+                chr_set.remove(s[j])
+                j += 1
+            chr_set.add(s[i])
+            maximum = max(maximum, i-j+1)
+            i += 1
+        return maximum
+
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
         """
