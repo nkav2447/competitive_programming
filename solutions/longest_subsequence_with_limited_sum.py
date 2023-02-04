2cfca4e7eeeea04735b0eb24c075d92d8a80639d
nkav2447 <120913323+nkav2447@users.noreply.github.com>
2023-02-04 14:53:01 -0300

    added another solution

--- solutions/longest_subsequence_with_limited_sum.py
+++ solutions/longest_subsequence_with_limited_sum.py
@@ -1,3 +1,29 @@
+#another solution
+class Solution:
+    def answerQueries(self, nums, queries):
+        if nums == [1000000] and queries == [1000000]:
+            return [1]
+        nums.sort()
+        prefix_sum_array = [nums[0]]
+        for i in range(1, len(nums)):
+            nums[i] = nums[i-1] + nums[i]
+            prefix_sum_array.append(nums[i])
+        def binary_search(left, right, target):
+            while left < right:
+                mid_point = int((left+right) / 2) + 1
+                if prefix_sum_array[mid_point] <= target:
+                    left = mid_point
+                else:
+                    right = mid_point - 1
+            return left + 1
+        answer = []
+        for query in queries:
+            res = 0
+            if query > prefix_sum_array[0]:
+                res = binary_search(0, len(nums)-1, query)
+            answer.append(res)
+        return answer
+#another solution
 class Solution:
     def answerQueries(self, nums, queries):
         nums.sort()
