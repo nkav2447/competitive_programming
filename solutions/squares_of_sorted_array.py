a65c7b9ea78bd5fe54cb09fdbe1e580a46ed835a
nkav2447 <120913323+nkav2447@users.noreply.github.com>
2023-02-05 18:28:11 -0300

    Update squares_of_sorted_array.py

--- solutions/squares_of_sorted_array.py
+++ solutions/squares_of_sorted_array.py
@@ -1,3 +1,18 @@
+#another solution
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
         return sorted([num**2 for num in nums])
+    
+ #another solution   
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        ans = []
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            if nums[left]**2 > nums[right]**2:
+                ans.append(nums[left]**2)
+                left += 1
+            else:
+                ans.append(nums[right]**2)
+                right -= 1
+        return ans[::-1]
