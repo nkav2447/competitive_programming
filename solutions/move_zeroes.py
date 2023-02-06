19775610ee9dbf59a44e9520a96d3339d7d1680d
nkav2447 <120913323+nkav2447@users.noreply.github.com>
2023-02-06 07:13:05 -0300

    added another solution

--- solutions/move_zeroes.py
+++ solutions/move_zeroes.py
@@ -1,3 +1,19 @@
+#added another solution
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        slow, fast = 0, 1
+        while fast < len(nums):
+            if nums[slow] != 0:
+                slow += 1
+                fast += 1
+            elif nums[fast] == 0 :
+                fast += 1
+            else:
+                nums[slow], nums[fast] = nums[fast], nums[slow]
+#added another solution
 class Solution(object):
     def moveZeroes(self, nums):
         """
