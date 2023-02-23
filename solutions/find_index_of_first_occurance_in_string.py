cecbfca118e77a201f5c47178eadc7887b9a00d1
nkav2447 <120913323+nkav2447@users.noreply.github.com>
2023-02-23 08:15:34 -0300

    another solution

--- solutions/find_index_of_first_occurance_in_string.py
+++ solutions/find_index_of_first_occurance_in_string.py
@@ -1,3 +1,15 @@
+#another solution
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle == 0:
+            return -1
+        if haystack == needle:
+            return 0
+        for i in range(len(haystack)):
+            if haystack[i : i + len(needle)] == needle:
+                return i
+        return -1
+ #another solution
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         return haystack.find(needle)
