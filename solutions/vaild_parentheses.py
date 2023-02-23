806480edf0faca1238f2aac8e42a7171b6809403
nkav2447 <120913323+nkav2447@users.noreply.github.com>
2023-02-23 08:02:51 -0300

    added another solution

--- solutions/vaild_parentheses.py
+++ solutions/vaild_parentheses.py
@@ -1,3 +1,20 @@
+#added another solution
+class Solution:
+    def isValid(self, s: str) -> bool:
+        brackets = {")": "(", "]": "[", "}": "{"}
+        opened = "({["
+        closed = ")}]"
+        stack = []
+        for c in s:
+            if c in opened:
+                stack.append(c)
+            elif c in closed:
+                if stack and stack[-1] == brackets[c]:
+                    stack.pop()
+                else:
+                    return False
+        return True if len(stack) == 0 else False
+#added another solution
 class Solution(object):
     def isValid(self, s):
         """
