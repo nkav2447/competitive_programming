d0dfcfa2864ae7d40708b1dbc6f4ec8745889a36
nkav2447 <120913323+nkav2447@users.noreply.github.com>
2023-02-25 13:57:48 -0300

    added another solution

--- solutions/length_of_last_word.py
+++ solutions/length_of_last_word.py
@@ -1,3 +1,16 @@
+#another solution
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        length, n = 0, len(s) - 1
+        while s[n] == " ":
+            n -= 1
+        for i in range(n, -1, -1):
+            if s[i] == " ":
+                return length
+            else:
+                length += 1
+        return length
+#another solution
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         return len(s.split()[-1])
