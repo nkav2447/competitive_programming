e27a22f22c3998b69470e21f340619530bf6dba1
nkav2447 <120913323+nkav2447@users.noreply.github.com>
2023-03-07 05:18:30 +0000

    added another solution

--- solutions/ransom_note.py
+++ solutions/ransom_note.py
@@ -1,3 +1,17 @@
+#another solution
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        ransomNoteCtr = {}
+        magazineCtr = {}
+        for c in ransomNote:
+            ransomNoteCtr[c] = ransomNoteCtr.get(c, 0) + 1
+        for c in magazine:
+            magazineCtr[c] = magazineCtr.get(c, 0) + 1
+        for c in ransomNote:
+            if c not in magazineCtr or magazineCtr[c] < ransomNoteCtr[c]:
+                return False
+        return True
+#another solution
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         magazineCtr = {}
