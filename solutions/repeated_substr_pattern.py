99c6a3e7643f07eae03569f7adec817e0488f858
nkav2447 <120913323+nkav2447@users.noreply.github.com>
2023-03-12 00:05:53 -0300

    added another solution

--- solutions/repeated_substr_pattern.py
+++ solutions/repeated_substr_pattern.py
@@ -1,3 +1,15 @@
+#added another solution
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        rep = ""
+        n = len(s)
+        for i in range(n // 2):
+            rep += s[i]
+            if n % len(rep) == 0:
+                if rep * (n // len(rep)) == s:
+                    return True
+        return False
+#added another solution
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
         n = len(s)
