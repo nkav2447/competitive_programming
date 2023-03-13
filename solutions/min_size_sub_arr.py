267f73229ecf3a6da6fb0463f531a718d9cbe63c
nkav2447 <120913323+nkav2447@users.noreply.github.com>
2023-03-13 09:46:19 -0300

    added another solution

--- solutions/min_size_sub_arr.py
+++ solutions/min_size_sub_arr.py
@@ -1,3 +1,18 @@
+#another solution
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        min_length = float('inf')
+        window_sum, window_start = 0, 0
+        for window_end in range(len(nums)):
+            window_sum += nums[window_end] 
+            while window_sum >= target:
+                min_length = min(min_length, window_end-window_start+1)
+                window_sum -= nums[window_start]
+                window_start += 1
+        if min_length == float('inf'):
+            return 0
+        return min_length
+#another solution
 class Solution(object):
     def minSubArrayLen(self, target, nums):
         """
