b9b1f345b4f285a8cb265295f3f9534597ce85e0
nkav2447 <120913323+nkav2447@users.noreply.github.com>
2023-03-17 02:13:00 -0300

    added another solution

--- solutions/fruit_in_to_baskets.py
+++ solutions/fruit_in_to_baskets.py
@@ -1,3 +1,22 @@
+#another solution
+class Solution:
+            def totalFruit(self, fruits: List[int]) -> int:
+                max_length, window_start = 0, 0
+                char_frequency = {}
+                for window_end in range(len(fruits)):
+                    right_char = fruits[window_end]
+                    if right_char not in char_frequency:
+                        char_frequency[right_char] = 0
+                    char_frequency[right_char] += 1
+                    while len(char_frequency) > 2:
+                        left_char = fruits[window_start]
+                        char_frequency[left_char] -= 1
+                        if char_frequency[left_char] == 0:
+                            del char_frequency[left_char]
+                        window_start += 1  
+                    max_length = max(max_length, window_end - window_start + 1)
+                return max_length
+#another solution
 class Solution(object):
     def totalFruit(self, fruits):
         """
