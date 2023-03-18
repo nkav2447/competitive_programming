1e7e30d2510b8327ea8aa6c363e96361de39d433
nkav2447 <120913323+nkav2447@users.noreply.github.com>
2023-03-18 05:23:18 -0300

    added another solution

--- solutions/longest_substr_without_rep.py
+++ solutions/longest_substr_without_rep.py
@@ -1,3 +1,22 @@
+#another solution
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        max_length, window_start = 0, 0
+        char_frequency = {}
+        for window_end in range(len(s)):
+            right_char = s[window_end]
+            if right_char not in char_frequency:
+                char_frequency[right_char] = 0
+            char_frequency[right_char] += 1
+            while char_frequency[right_char] > 1:
+                left_char = s[window_start]
+                char_frequency[left_char] -= 1
+                if char_frequency[left_char] == 0:
+                    del char_frequency[left_char]
+                window_start += 1 
+            max_length = max(max_length, window_end - window_start + 1)
+        return max_length
+#another solution
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         if s == None or len(s) == 0:
@@ -12,7 +31,7 @@ class Solution:
             maximum = max(maximum, i-j+1)
             i += 1
         return maximum
-
+#another solution
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
         """
