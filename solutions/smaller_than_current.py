05d572215fdb08eec2190ea5cf67bffbbfb64fe8
nkav2447 <120913323+nkav2447@users.noreply.github.com>
2023-03-22 01:01:28 -0300

    another solution

--- solutions/smaller_than_current.py
+++ solutions/smaller_than_current.py
@@ -1,3 +1,14 @@
+#another solution
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        res = []
+        for num in nums:
+            current = [x for x in nums if x < num]
+            res.append(len(current))
+        return res
+#another solution
 class Solution(object):
     def smallerNumbersThanCurrent(self, nums):
         return list(map(lambda n: sorted(nums).index(n), nums))
+    
+
