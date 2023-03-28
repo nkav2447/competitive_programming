432cbac9dbcad787e277b9b4bccbeb233df2b623
nkav2447 <120913323+nkav2447@users.noreply.github.com>
2023-03-28 02:01:40 -0300

    Update 3sum.py

--- solutions/3sum.py
+++ solutions/3sum.py
@@ -1,3 +1,23 @@
+#added another solution
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums = sorted(nums)
+        res = set()
+        for idx, num in enumerate(nums):
+            target = -nums[idx]
+            i, n = idx + 1, len(nums) - 1
+            while i < n:
+                two_sum = nums[i] + nums[n]
+                if two_sum < target:
+                    i += 1
+                elif two_sum > target:
+                    n -= 1
+                else:
+                    res.add((num, nums[i], nums[n]))
+                    i += 1
+                    n -= 1
+        return res
+#added another solution
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
