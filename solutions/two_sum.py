4edd40d72b70de4656fd4236d0983ca41f615fde
nkav2447 <120913323+nkav2447@users.noreply.github.com>
2023-04-01 01:10:27 -0300

    added another solution

--- solutions/two_sum.py
+++ solutions/two_sum.py
@@ -1,3 +1,13 @@
+#another solution
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_idx = {}  
+        for i, num in enumerate(nums):
+            if target - num in num_idx:
+                return [num_idx[target - num], i]
+            else:
+                num_idx[num] = i
+#another solution
 class Solution(object):
     def twoSum(self, nums, target):
         """
