33da11f1e3646c95102d968a01742a46f71d2482
nkav2447 <120913323+nkav2447@users.noreply.github.com>
2023-04-12 13:05:49 -0300

    Update add_strings.py

--- solutions/add_strings.py
+++ solutions/add_strings.py
@@ -1,3 +1,20 @@
+#another solution
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        num1 = list(num1)
+        num2 = list(num2)
+        if len(num1) > len(num2):
+            for i in range(len(num1) - len(num2)):
+                num2.insert(0,"0")
+        elif len(num1) < len(num2):
+            for i in range(len(num2) - len(num1)):
+                num1.insert(0,"0")
+        res = 0 
+        for i in range(len(num1)):
+            temp = (ord(num1.pop()) + ord(num2.pop()))%48
+            res += temp * 10**i
+        return str(res)
+#another solution
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
         added = ""
